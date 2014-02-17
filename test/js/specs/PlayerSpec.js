4e8a1637bb61f6e1438616ce840ea5d8874a95e2
Linda Goldstein <lgoldste@thoughtworks.com>
2014-02-16 17:24:22 -0800

    works in intellij

---
+++ test/js/specs/PlayerSpec.js
@@ -0,0 +1,13 @@
+describe("Player", function () {
+
+
+    it("should be able to play a Song", function () {
+//        var player = new Player();
+//        var song = new Song();
+//        player.play(song);
+//        expect(player.currentlyPlayingSong).toEqual(song);
+
+        expect(true).toEqual(true);
+
+    });
+});
